be341963fe6c208e2f29f9824d2b898c78e2d497
Davi Brasi <davi.brasil2005@gmail.com>
2026-07-16 19:46:26 -0400

    Initial Commit

---
+++ SwiftDMG/SwiftDMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDMGApp.swift
+//  SwiftDMG
+//
+//  Created by Davi Sobreira Brasil on 16/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
