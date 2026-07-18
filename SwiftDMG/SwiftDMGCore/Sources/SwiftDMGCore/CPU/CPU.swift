1cb88f6daa8d0da1eff18d3ef94dd8223b4547ec
Davi Brasi <davi.brasil2005@gmail.com>
2026-07-17 22:07:46 -0400

    feat: cpu initializer

--- SwiftDMG/SwiftDMGCore/Sources/SwiftDMGCore/CPU/CPU.swift
+++ SwiftDMG/SwiftDMGCore/Sources/SwiftDMGCore/CPU/CPU.swift
@@ -5,3 +5,25 @@
 //  Created by Davi Sobreira Brasil on 16/07/26.
 //
 
+import Foundation
+
+public class CPU {
+    public var registers = Registers()
+    
+    private let bus: Addressable
+    
+    public init(bus: Addressable){
+        self.bus = bus
+        reset()
+    }
+    
+    private func reset() {
+        // Os valores padrão do emulador ao iniciar o jogo
+        registers.af = 0x01B0
+        registers.bc = 0x0013
+        registers.de = 0x00D8
+        registers.hl = 0x014D
+        registers.sp = 0xFFFE
+        registers.pc = 0x0100
+    }
+}
